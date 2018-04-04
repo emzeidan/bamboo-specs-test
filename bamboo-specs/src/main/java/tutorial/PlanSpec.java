5cd0b550116e6030fcfe66450c6f43d6c2d9d5cc
Elias Zeidan <ezeidan@atlassian.com>
2018-04-04 16:12:39 +0000

    First Specs run -- testing

--- bamboo-specs/src/main/java/tutorial/PlanSpec.java
+++ bamboo-specs/src/main/java/tutorial/PlanSpec.java
@@ -1,13 +1,43 @@
 package tutorial;
 
 import com.atlassian.bamboo.specs.api.BambooSpec;
+import com.atlassian.bamboo.specs.api.builders.BambooKey;
+import com.atlassian.bamboo.specs.api.builders.BambooOid;
+import com.atlassian.bamboo.specs.api.builders.Variable;
+import com.atlassian.bamboo.specs.api.builders.notification.Notification;
+import com.atlassian.bamboo.specs.api.builders.permission.PermissionType;
+import com.atlassian.bamboo.specs.api.builders.permission.Permissions;
+import com.atlassian.bamboo.specs.api.builders.permission.PlanPermissions;
+import com.atlassian.bamboo.specs.api.builders.plan.Job;
 import com.atlassian.bamboo.specs.api.builders.plan.Plan;
 import com.atlassian.bamboo.specs.api.builders.plan.PlanIdentifier;
+import com.atlassian.bamboo.specs.api.builders.plan.Stage;
+import com.atlassian.bamboo.specs.api.builders.plan.artifact.Artifact;
+import com.atlassian.bamboo.specs.api.builders.plan.branches.BranchCleanup;
+import com.atlassian.bamboo.specs.api.builders.plan.branches.PlanBranchManagement;
+import com.atlassian.bamboo.specs.api.builders.plan.configuration.AllOtherPluginsConfiguration;
+import com.atlassian.bamboo.specs.api.builders.plan.configuration.ConcurrentBuilds;
 import com.atlassian.bamboo.specs.api.builders.project.Project;
+import com.atlassian.bamboo.specs.api.builders.repository.VcsChangeDetection;
+import com.atlassian.bamboo.specs.api.builders.repository.VcsRepositoryIdentifier;
+import com.atlassian.bamboo.specs.api.builders.trigger.RepositoryBasedTrigger;
+import com.atlassian.bamboo.specs.builders.notification.CommentAddedNotification;
+import com.atlassian.bamboo.specs.builders.notification.EmailRecipient;
+import com.atlassian.bamboo.specs.builders.notification.JobCompletedNotification;
+import com.atlassian.bamboo.specs.builders.notification.PlanCompletedNotification;
+import com.atlassian.bamboo.specs.builders.notification.UserRecipient;
+import com.atlassian.bamboo.specs.builders.repository.bitbucket.cloud.BitbucketCloudRepository;
+import com.atlassian.bamboo.specs.builders.repository.git.UserPasswordAuthentication;
+import com.atlassian.bamboo.specs.builders.repository.viewer.BitbucketCloudRepositoryViewer;
+import com.atlassian.bamboo.specs.builders.task.CheckoutItem;
+import com.atlassian.bamboo.specs.builders.task.CommandTask;
+import com.atlassian.bamboo.specs.builders.task.MavenTask;
+import com.atlassian.bamboo.specs.builders.task.ScriptTask;
+import com.atlassian.bamboo.specs.builders.task.VcsCheckoutTask;
+import com.atlassian.bamboo.specs.builders.trigger.BitbucketServerTrigger;
+import com.atlassian.bamboo.specs.builders.trigger.RemoteTrigger;
 import com.atlassian.bamboo.specs.util.BambooServer;
-import com.atlassian.bamboo.specs.api.builders.permission.Permissions;
-import com.atlassian.bamboo.specs.api.builders.permission.PermissionType;
-import com.atlassian.bamboo.specs.api.builders.permission.PlanPermissions;
+import com.atlassian.bamboo.specs.util.MapBuilder;
 
 /**
  * Plan configuration for Bamboo.
@@ -50,6 +80,11 @@ public class PlanSpec {
 
     Plan createPlan() {
         return new Plan(project(), "Test Plan", "TEST")
-                .description("Plan created from Bamboo Java Specs in GitHub");
+                .description("Plan created from Bamboo Java Specs in GitHub")
+	    .stages(new Stage("Default Stage")
+		    .jobs(new Job("Default Job"),
+			  new BambooKey("GHJOB1"))
+		    .tasks(new ScriptTask()
+			   .inlineBody("echo foo\ndate\necho 'hello world'")));
     }
 }
