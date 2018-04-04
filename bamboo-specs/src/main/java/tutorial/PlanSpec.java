ed1ed16a56992177ea982ecb937a4d685df3c401
Elias Zeidan <ezeidan@atlassian.com>
2018-04-04 16:15:24 +0000

    Specs run -- testing

--- bamboo-specs/src/main/java/tutorial/PlanSpec.java
+++ bamboo-specs/src/main/java/tutorial/PlanSpec.java
@@ -85,6 +85,6 @@ public class PlanSpec {
 		    .jobs(new Job("Default Job"),
 			  new BambooKey("GHJOB1"))
 		    .tasks(new ScriptTask()
-			   .inlineBody("echo foo\ndate\necho 'hello world'")));
+			   .inlineBody("echo 'foo bar'\ndate\necho 'hello world'")));
     }
 }
